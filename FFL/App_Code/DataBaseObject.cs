dedee7922f59e66c03b56db5a999873a6c46d16d
laurenmoliver <lmo38@nau.edu>
2013-09-12 00:20:06 -0700

    Creating website foundation/architecture

---
+++ FFL/App_Code/DataBaseObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace DataBaseObject
+{
+    ///<remarks>
+    /// Summary: Current Data Access
+    /// Sponsor: CIS330 Fall 2013 
+    /// Creation Date: August 3, 2013
+    /// Notes: 
+    /// </remarks>
+
+    public class DataBaseObject
+    {
+
+        private SqlConnection c_Connection;
+        public SqlConnection Connection
+        {
+            get
+            {
+                return c_Connection;
+            }
+            set
+            {
+                c_Connection = value;
+            }
+        }
+
+        public DataBaseObject(string newConnectionString)
+	    {
+            Connection = new SqlConnection(newConnectionString);
+	    }
+    }
+}
---
+++ FFL/App_Code/headerLayout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App_Code.headerLayout
+{
+    /// <summary>
+    /// This is where we will generate page initialization
+    /// and nav logic here
+    /// and html
+    /// </summary>
+    public class HeaderLayout
+    {
+        public HeaderLayout()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
